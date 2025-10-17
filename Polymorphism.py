9288057cda5b3f8b86966264f65944e1d52b30aa
N.V.H.S Charitha_sree <nvhsc14@gmail.com>
2025-10-17 10:48:40 +0530

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,11 @@
+#Use len() with a string
+my_string = "hello"
+print(len(my_string))
+
+#Use len() with a list
+my_list = (1,2,3,4)
+print(len(my_list))
+
+#Use len() with a dictionary
+my_dict = {"a":1,"b":2}
+print(len(my_dict))
---
+++ PolymorphismCM.py
@@ -0,0 +1,17 @@
+class Dog:
+    def sound(self):
+        print("Woof!")
+
+class Cat:
+    def sound(self):
+        print("Meow!")
+
+#Make objects (instances) from these classes
+
+my_dog=Dog()
+my_cat=Cat()
+
+#Call the same method on different objects
+
+my_dog.sound()
+my_cat.sound()
